d54b263934a011160413e07b93fad5d268141c8e
terryalexis <70789551+terryalexis@users.noreply.github.com>
2023-12-19 15:57:37 -0700

    form validation

--- register.php
+++ register.php
@@ -3,31 +3,69 @@ header("Content-type: text/plain; charset=UTF-8");
 main();
 
 function main() {
-    $dog_detail_keys = [
-        'dog_name',
-        'breed',
-        'gender',
-        'microchip_number'
-    ];
-    $dog_details = getDogDetails($dog_detail_keys);
-    
-    $concat_dog_detail_keys = implode(', ', array_keys($dog_detail_keys));
-    $concat_dog_detail_values = "'". implode(", '", array_values($dog_details)) . "'";
+    $isValid = true;
 
-    $sql_query = "INSERT INTO Dogs ($concat_dog_detail_keys) VALUES ($concat_dog_details);";
+    // Dog Name Validation
+    $dog_name = '';
+    $dog_name_err = '';
+    if(empty($_POST['dog_name'])) {
+        $dog_name_err = 'Dog name is required';
+        $isValid = false;
+    } else {
+        $dog_name = trim($_POST['dog_name']);
+    }
+
+    // Breed Validation
+    $breed = '';
+    $breed_err = '';
+    if(empty($_POST['breed'])){
+        $breed_err = 'Breed is required';
+        $isValid = false;
+    } else {
+        $breed = trim($_POST['breed']);
+    }
+
+    // Gender Validation
+    $gender = '';
+    $gender_err = '';
+    if(empty($_POST['gender'])) {
+        $gender_err = 'Gender is required';
+        $isValid = false;
+    } else {
+        $gender = trim($_POST['gender']);
+    }
+
+    // Microchip Number Validation
+    $microchip_num = '';
+    $microchip_num_err = '';
+    if(empty($_POST['microchip_number'])) {
+        $microchip_num_err = null;
+    } else {
+        $microchip_number = $_POST['microchip_number'];
+    }
     
-    $success = true;
-    
-    echo($success);
+    $sql_query = "INSERT INTO Dogs (dog_name, breed, gender, microchip_number) 
+    VALUES ('$dog_name', '$breed', '$gender', $microchip_number);";
+
+    if ($isValid) {
+        echo(insertIntoFakeDatabase($sql_query));
+    } else {
+        echo(false);
+    }
 }
+
 
 function getDogDetails($dog_detail_keys) {
     $dog_details = [];
     foreach($dog_detail_keys as $dog_detail_key) {
         $dog_details[$dog_detail_key] = $_POST[$dog_detail_key];
     }
-
     return $dog_details;
+}
+
+
+function insertIntoFakeDatabase($sql_query) {
+    return true;
 }
 
 ?>
--- registration_form.js
+++ registration_form.js
@@ -1,6 +1,10 @@
 $(document).ready(function (){
     $('#dog-registration-form').on('submit', function(e){
         e.preventDefault();
+
+        checkDogName();
+        checkBreed();
+        checkMicrochipNum();
         
         var data = {
             dog_name: $('#dog-name').val(),
@@ -13,12 +17,57 @@ $(document).ready(function (){
             type: 'post',
             url: 'register.php',
             data: data,
+            beforeSend: function () {
+                $('#register-dog').attr("disabled", true);
+            },
             success: function(res){
                 console.log(res);
             },
-            error: function() {
-                alert('Failed to register');
+            error: function(e) {
+                alert('Error Registering');
+            },
+            complete: function () {
+                setTimeout(function () {
+                    $('#dog-registration-form').trigger("reset");
+                    $('#register-dog').attr("disabled", false);
+                }, 50000);
             }
         });
     });
 })
+
+function checkDogName() {
+    if ($('#dog-name').val().length < 1) {
+        $('#dog-name-err').html('Dog name is required');
+        return false;
+    } else {
+        $('#dog-name-err').html('');
+        return true;
+    }
+}
+
+function checkBreed() {
+    var pattern = /^[a-zA-Z\s]*$/;
+    var breed = $('#breed').val();
+    var valid_breed = pattern.test(breed);
+    if ($('#breed').val().length < 2) {
+        $('#breed-err').html('Breed is required');
+        return false;
+    } else if (!valid_breed) {
+        $('#breed-err').html('Breed can only include letters and spaces');
+        return false;
+    } else {
+        $('#dog-name-err').html('');
+        return true;
+    }
+}
+
+function checkMicrochipNum() {
+    if ($('#microchip-number').val().length > 20) {
+        $('#microchip-num-err').html('Microchip # cannot be longer than 20 digits');
+        return false;
+    } else {
+        $('#microchip-num-err').html('');
+        return true;
+    }
+}
