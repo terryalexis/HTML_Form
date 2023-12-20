decfc74c61d926d786c3a482f0337bcddd35d41f
terryalexis <70789551+terryalexis@users.noreply.github.com>
2023-12-19 20:50:03 -0700

    finished front-end validation

--- registration_form.js
+++ registration_form.js
@@ -2,43 +2,25 @@ $(document).ready(function (){
     $('#dog-registration-form').on('submit', function(e){
         e.preventDefault();
 
-        checkDogName();
-        checkBreed();
-        checkMicrochipNum();
-        
-        var data = {
-            dog_name: $('#dog-name').val(),
-            breed: $('#breed').val(),
-            gender: $('input[name="gender"]:checked').val(),
-            microchip_number: $('#microchip-number').val()
-        };
-        
-        $.ajax({
-            type: 'post',
-            url: 'register.php',
-            data: data,
-            beforeSend: function () {
-                $('#register-dog').attr("disabled", true);
-            },
-            success: function(res){
-                console.log(res);
-            },
-            error: function(e) {
-                alert('Error Registering');
-            },
-            complete: function () {
-                setTimeout(function () {
-                    $('#dog-registration-form').trigger("reset");
-                    $('#register-dog').attr("disabled", false);
-                }, 50000);
-            }
-        });
+        is_valid = checkDogName() && checkBreed() && checkMicrochipNum();
+
+        if(is_valid) {
+            sendRegistrationForm();
+        }
     });
 })
 
 function checkDogName() {
-    if ($('#dog-name').val().length < 1) {
+    var dog_name = $('#dog-name').val();
+    var pattern = /^[a-zA-Z-'\s]*$/;
+    if (dog_name.length < 1) {
         $('#dog-name-err').html('Dog name is required');
+        return false;
+    } else if (!pattern.test(dog_name)) {
+        $('#dog-name-err').html('No digits or special characters allowed besides \' and -');
+        return false;
+    } else if (dog_name.length > 50) {
+        $('#dog-name-err').html('Name must be shorter than 50 characters');
         return false;
     } else {
         $('#dog-name-err').html('');
@@ -47,14 +29,16 @@ function checkDogName() {
 }
 
 function checkBreed() {
-    var pattern = /^[a-zA-Z\s]*$/;
     var breed = $('#breed').val();
-    var valid_breed = pattern.test(breed);
-    if ($('#breed').val().length < 2) {
-        $('#breed-err').html('Breed is required');
+    var pattern = /^[a-zA-Z\s]*$/;
+    if (breed.length < 2) {
+        $('#breed-err').html('Breed must be longer than 2 characters');
         return false;
-    } else if (!valid_breed) {
-        $('#breed-err').html('Breed can only include letters and spaces');
+    } else if (breed.length > 50) {
+        $('#breed-err').html('Breed must be shorter than 50 characters');
+        return false;
+    } else if (!pattern.test(breed)) {
+        $('#breed-err').html('Only letters and spaces allowed');
         return false;
     } else {
         $('#dog-name-err').html('');
@@ -63,11 +47,58 @@ function checkBreed() {
 }
 
 function checkMicrochipNum() {
-    if ($('#microchip-number').val().length > 20) {
-        $('#microchip-num-err').html('Microchip # cannot be longer than 20 digits');
-        return false;
+    var microchip_number = $('#microchip-number').val();
+    var pattern = /^[0-9]*$/;
+    if (microchip_number.length > 1) {
+        if (!pattern.test(microchip_number)) {
+            $('#microchip-num-err').html('Microchip # must only be digits 0-9');
+            return false;
+        } else if (microchip_number.length < 9) {
+            $('#microchip-num-err').html('Microchip # cannot be shorter than 9 digits');
+            return false;
+        } else if (microchip_number.length > 15) {
+            $('#microchip-num-err').html('Microchip # cannot be longer than 15 digits');
+            return false; 
+        } else {
+            $('#microchip-num-err').html('');
+            return true;
+        }
     } else {
         $('#microchip-num-err').html('');
         return true;
     }
+}
+
+function sendRegistrationForm() {
+    var data = {
+        dog_name: $('#dog-name').val(),
+        breed: $('#breed').val(),
+        gender: $('input[name="gender"]:checked').val(),
+        microchip_number: $('#microchip-number').val()
+    };
+
+    $.ajax({
+        type: 'post',
+        url: 'register.php',
+        data: data,
+        beforeSend: function () {
+            $('#register-dog').attr("disabled", true);
+        },
+        success: function(){
+            $('.container-content').html('\
+                <div class="m-5">\
+                    <h2>Registration Submitted!</h2>\
+                    <a href="index.html">Register Another Dog</a>\
+                </div>');
+        },
+        error: function() {
+            $('.alert').removeAttr('hidden');
+        },
+        complete: function () {
+            setTimeout(function () {
+                $('#dog-registration-form').trigger("reset");
+                $('#register-dog').attr("disabled", false);
+            }, 50000);
+        }
+    });
 }
